8462c5affcc6920b3a213eafaa614593e097731d
Sanghyun <114604135+ddingorang@users.noreply.github.com>
2026-02-02 17:19:01 +0900

    Title: Factorial, Time: 112ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260202/Factorial/factorial.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+
+    static int recur(int n) {
+        if(n==0) {
+            return 1;
+        }
+        else if(n==1) {
+            return 1;
+        }
+        return n*recur(n-1);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.println(recur(n));
+    }
+}
