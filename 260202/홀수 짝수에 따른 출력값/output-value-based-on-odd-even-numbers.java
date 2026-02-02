082abab6bd0e285259776fbbf8e2ef59bd7865ac
Sanghyun <114604135+ddingorang@users.noreply.github.com>
2026-02-02 17:24:04 +0900

    Title: 홀수 짝수에 따른 출력값, Time: 116ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 260202/홀수 짝수에 따른 출력값/output-value-based-on-odd-even-numbers.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Main {
+    static int recur(int n) {
+        if(n == 1) {
+            return 1;
+        }
+        else if(n==2) {
+            return 2;
+        }
+        return n + recur(n-2);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        // Please write your code here.
+        System.out.println(recur(n));
+    }
+}
